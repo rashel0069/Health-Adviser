4b59081a5c6af32a3082ba71bd782dc8d8a98301
RashelHassan <rabbi0069@gmail.com>
2019-07-24 02:33:10 +0600

    Health Adviser

--- app/src/main/java/com/appshat/health_tutor/NearbyHospitalFragment.java
+++ app/src/main/java/com/appshat/health_tutor/NearbyHospitalFragment.java
@@ -7,13 +7,22 @@ import android.os.Bundle;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
-public class NearbyHospitalFragment extends Fragment {
+
+public class NearbyHospitalFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
 
     public NearbyHospitalFragment(){
         //required empty public constructor
@@ -24,6 +33,28 @@ public class NearbyHospitalFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_nearby_hospital,container,false);
         return v;
     }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.usermap);
+        mapFragment.getMapAsync(this);
+
+    }
 
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+        int reqCode = 1;
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(getActivity(), new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, reqCode);
+            return;
+        }
+        LatLng GUB = new LatLng(23.786920, 90.377537);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(GUB,15));
+        mMap.setMyLocationEnabled(true);
+    }
 }
--- app/src/main/java/com/appshat/health_tutor/NewBornbabyFragment.java
+++ app/src/main/java/com/appshat/health_tutor/NewBornbabyFragment.java
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
+//AIzaSyCPZG1egPoXttCA67DziavzuF99wrKnRY0
 public class NewBornbabyFragment extends Fragment {
     @Nullable
     @Override
